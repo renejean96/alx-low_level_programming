eeac8fb21d0f5dd8ce432660cd1b84e2c36f9d1b
renejean96 <jeanmrene8@gmail.com>
2023-02-16 11:21:39 -0800

    a c program that print a text using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main = Entry point
- *
+ * Description : 'this program print a text using puts'
  * return: Always 0 (success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
