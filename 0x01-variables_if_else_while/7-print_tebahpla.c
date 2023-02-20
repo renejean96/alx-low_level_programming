c6749eb8be732427f186d8fa152556d6bae2417f
renejean96 <jeanmrene8@gmail.com>
2023-02-19 20:47:33 -0800

    print lower case alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char first_char;
+
+	for (first_char = 'z'; first_char >= 'a'; first_char--)
+		putchar(first_char);
+		putchar('\n');
+
+	return (0);
+}	i
